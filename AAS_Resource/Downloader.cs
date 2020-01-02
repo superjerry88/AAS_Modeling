abb65f616b3e2b5978e7ddfc570747fe7bcca116
Jerry Hang <superjerry88@hotmail.com>
2020-01-02 19:00:08 +0800

    Added Resource Project

---
+++ AAS_Resource/Downloader.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace AAS_Resource
+{
+    class Downloader
+    {
+        public static string CheckNeedsUpdate()
+        {
+            using (var client = new WebClient())
+            {
+                return client.DownloadString("https://raw.githubusercontent.com/goaaats/universalis_act_plugin/master/version");
+
+            }
+        }
+    }
+}
